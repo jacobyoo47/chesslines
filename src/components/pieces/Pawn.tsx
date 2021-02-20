2c6ebd7d3d37f2f18620684b170c525ccbe3bf8f
godpng <jayoo@g.hmc.edu>
2021-02-20 14:26:41 -0800

    Fix pawn double-push

--- src/components/pieces/Pawn.tsx
+++ src/components/pieces/Pawn.tsx
@@ -1,8 +1,8 @@
 import Piece from './Piece'
 import { wPawn, bPawn } from '../../static/pieceIcons'
 
-const wPawnInitial = [48,49,50,51,52,53,54,55]
-const bPawnInitial = [8,9,10,11,12,13,14,15]
+const wPawnInitial = [48, 49, 50, 51, 52, 53, 54, 55]
+const bPawnInitial = [8, 9, 10, 11, 12, 13, 14, 15]
 
 export default class Pawn extends Piece {
   constructor(player: string) {
@@ -14,31 +14,20 @@ export default class Pawn extends Piece {
   }
 
   isMovePossible(src: number, dest: number, isDestEnemyOccupied: boolean) {
-    if (this.player === 'white') {
-      if (
-        (dest === src - 8 && !isDestEnemyOccupied) ||
-        (dest === src - 16 && src in wPawnInitial)
-      ) {
-        return true
-      } else if (
-        isDestEnemyOccupied &&
-        (dest === src - 9 || dest === src - 7)
-      ) {
-        return true
-      }
-    } else if (this.player === 'black') {
-      if (
-        (dest === src + 8 && !isDestEnemyOccupied) ||
-        (dest === src + 16 && src in bPawnInitial)
-      ) {
-        return true
-      } else if (
-        isDestEnemyOccupied &&
-        (dest === src + 9 || dest === src + 7)
-      ) {
-        return true
-      }
+    const p = this.player === 'white' ? 1 : -1
+    const pawnInitial = this.player === 'white' ? wPawnInitial : bPawnInitial
+    if (
+      (dest === src - (8 * p) && !isDestEnemyOccupied) ||
+      (dest === src - (16 * p) && pawnInitial.includes(src) && !isDestEnemyOccupied)
+    ) {
+      return true
+    } else if (
+      isDestEnemyOccupied &&
+      (dest === src - (9 * p) || dest === src - (7 * p))
+    ) {
+      return true
     }
+
     return false
   }
 
